c02584da5be5dadd08c6da928b6d67d3711bba52
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-04 01:23:43 +0500

    Install react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        Hello World
+      </div>
+    </div>
+  );
+}
+
+export default App;
