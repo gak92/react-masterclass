de7cd60e10796f62f367d0aff18924d2344a4137
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-04 01:36:50 +0500

    play with app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 
 function App() {
+  let name = 'react-app';
+
+  console.log('App');
+
   return (
     <div className="App">
       <div className="App-header">
-        Hello World
+        Hello World {name}
+        <Demo></Demo>
+      </div>
+    </div>
+  );
+}
+
+function Demo() {
+
+  console.log('Demo');
+  
+  return (
+    <div className="App">
+      <div className="App-header">
+        Demo child
       </div>
     </div>
   );
